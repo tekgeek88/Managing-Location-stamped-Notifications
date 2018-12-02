e6d0ad655a9b9ccdfbc58b6f69caf9423b0748f1
Carl Argabright <carl.argabright@gmail.com>
2018-12-01 16:32:34 -0800

    Implemented BST outline - NON GENERIC

--- BST.java
+++ BST.java
@@ -1,1 +1,81 @@
-// BST Class
+public class BST<K, V> implements Map<Double, Map<Double, LocNot>> {
+
+	public BST(){
+		
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean full() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<Double, LocNot> retrieve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Map<Double, LocNot> e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean find(Double key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int nbKeyComp(Double key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean insert(Double key, Map<Double, LocNot> data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Double key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Pair<Double, Map<Double, LocNot>>> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pair<Double, Map<Double, LocNot>>> getRange(Double k1, Double k2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nbKeyComp(Double k1, Double k2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
+
+
